755ec269aba3e5a66b65d42acbcf864541f5f5e0
Vitor Freitas <vitordeavilafreitas@gmail.com>
2025-01-20 00:07:29 -0300

    feat: implementando constant colors no projeto, globalizando sistema de cores

---
+++ constants/colors.tsx
@@ -0,0 +1,12 @@
+const Colors = {
+  background: "#001534",
+  white: "#FAFAFB",
+  buttons: "#002254",
+  cards: "#002864",
+  black: "#000000",
+  placeholder: "#0015347b",
+  white40: "#fafafb67",
+  red: "#900000",
+};
+
+export default Colors;
